43bd75e8f26504009cd0adc18e5939bc0d5aec4e
meysam zohouri <meysamzohoori.mehriz@gmail.com>
2024-02-12 10:33:03 +0330

    feat: run the project

--- database/database.go
+++ database/database.go
@@ -2,7 +2,9 @@ package database
 
 import (
 	"crud-example-go/database/migrations"
+	"log"
 
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/jinzhu/gorm"
 )
 
@@ -10,9 +12,10 @@ var DB *gorm.DB
 
 func Init() {
 	var err error
-	DB, err = gorm.Open("splite3", "store")
-
+	DB, err = gorm.Open("mysql", "root:password@tcp(127.0.0.1:3306)/moochin?charset=utf8&parseTime=True&loc=Local")
+	println(err)
 	if err != nil {
+		log.Fatal(err)
 		panic("Failed to connect to data")
 	}
 
