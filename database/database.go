9e0f35262362297102d7e1390843fc814efa26c0
meysam zohouri <meysamzohoori.mehriz@gmail.com>
2024-02-12 10:38:54 +0330

    feat: database ok

--- database/database.go
+++ database/database.go
@@ -2,7 +2,6 @@ package database
 
 import (
 	"crud-example-go/database/migrations"
-	"log"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jinzhu/gorm"
@@ -15,10 +14,8 @@ func Init() {
 	DB, err = gorm.Open("mysql", "root:password@tcp(127.0.0.1:3306)/moochin?charset=utf8&parseTime=True&loc=Local")
 	println(err)
 	if err != nil {
-		log.Fatal(err)
 		panic("Failed to connect to data")
 	}
-
 	migrations.MigrateUser(DB)
 
 }
